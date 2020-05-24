5a28e2a841079f52c5d6922dcf4d4e21a5c6fb35
Daday Rivas <65819194+dayrivas@users.noreply.github.com>
2020-05-23 19:22:21 -0600

    Add files via upload
    index

---
+++ index.php
@@ -0,0 +1,18 @@
+  <!-- Header -->
+	<?php include('header.php'); ?>
+	
+	
+	<?php $rol = "rolAdmin" ?>
+
+  <!-- Formularios según perfil -->
+
+  <!-- Footer -->
+	<?php include('footer.php'); ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="cambiadisplay.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
