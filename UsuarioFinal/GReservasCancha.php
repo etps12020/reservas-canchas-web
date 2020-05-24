86fa420503f7d11da879ed101bb5d5093d3cc99f
Daday Rivas <65819194+dayrivas@users.noreply.github.com>
2020-05-23 21:40:06 -0600

    Add files via upload

---
+++ UsuarioFinal/GReservasCancha.php
@@ -0,0 +1,91 @@
+<html>
+    <title>Gestion de reservas</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+    $( function() {
+    $( "#datepicker" ).datepicker({buttonImage: "images/calendar.gif"});
+    } );
+    </script>
+
+
+    <div class="row styleRow1">
+            <div class="col-5"style="padding-left:25px">
+                <!--  <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Menú
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Reservaciones</a>
+                        <a class="dropdown-item" href="#">Usuarios</a>
+                        <a class="dropdown-item" href="#">Canchas</a>
+                        <a class="dropdown-item" href="#">Edificios</a>
+                        <a class="dropdown-item" href="#">Cerrar Sesion</a>
+                    </div> 
+                </div>  -->  
+            </div>
+			
+            
+                    
+        </div>
+         <h4 align="center">Bienvenido Usuario </h4> 
+           <br><h2 align="center" style="background-color:#8A084B"><font color=White>Reservaciones creadas</font></h2></br>
+</head>
+<body>
+<div class="container">
+        <div class="RoDate">
+        <div class="col">
+        <section>
+            <table class="table table-hover">
+                <!--<caption>List of users</caption>-->
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Usuario</th>
+                    <th scope="col">Horarios</th>
+                    <th scope="col">Canchas</th>
+                    <th scope="col">Gestion</th>
+                    
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td><p class="text-success">Aprobada</p></td>
+                    
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    
+                    <td><p class="text-danger">Rechazada</p></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td><p class="text-success">Aprobada</p></td>
+                    
+                    </tr>
+                </tbody>
+            </table>   
+        </section>     
+        </div>
+</div>
+       
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        </div>
+        <body>
+    
